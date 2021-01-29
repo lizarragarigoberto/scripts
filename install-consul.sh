651fa1af297e4e9d134d4164dc49f83f1641172a
lizarragarigoberto <lizarraga.rigoberto@gmail.com>
2021-01-29 01:27:04 -0600

    Create install-consul.sh
    install consul agent and register

---
+++ install-consul.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+echo "##### DESCARGAR ARCHIVO #####"
+#yum install curl unzip jq -y
+export CONSUL_VERSION="1.9.2"
+export CONSUL_URL="https://releases.hashicorp.com/consul"
+curl --silent --remote-name \
+  ${CONSUL_URL}/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip
+curl --silent --remote-name \
+  ${CONSUL_URL}/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_SHA256SUMS.sig
+unzip consul_${CONSUL_VERSION}_linux_amd64.zip
+echo "### move binaries ###"
+sudo mv consul /usr/bin/
+
+echo "###### CREATE DIRECTORIES ######"
+useradd -m -d /opt/consul consul
+mkdir -m 755 /etc/consul.d
+mkdir -m 755 /opt/consul/logs
+
+echo "#### CONFIGS ####"
+realip=$(hostname -I | awk '{print $2}')
+cat << EOF > /etc/consul.d/consul.hcl
+datacenter = "dc1"
+data_dir = "/opt/consul"
+start_join = ["192.168.0.10"]
+bind_addr = "$realip"
+EOF
+
+
+cat << EOF > /etc/consul.d/web.json
+{
+   "service":{
+     "name": "web",
+     "port": 80
+    }
+}
+EOF
+
+chown -R consul:consul /opt/consul
+chown -R consul: /opt/consul.d
+chown -R consul:consul /etc/consul.d
+sudo chmod 755 /opt/consul/*
+
+cat << EOF > /etc/systemd/system/consul.service
+[Unit]
+Description="HashiCorp Consul - A service mesh solution"
+Documentation=https://www.consul.io/
+Requires=network-online.target
+After=network-online.target
+ConditionFileNotEmpty=/etc/consul.d/consul.hcl
+
+[Service]
+User=consul
+Group=consul
+ExecStart=/usr/bin/consul agent -config-dir=/etc/consul.d/
+ExecReload=/usr/bin/consul reload
+KillMode=process
+Restart=always
+LimitNOFILE=65536
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+echo "#### VALIDATING #####"
+consul validate /etc/consul.d/consul.hcl
+
+
+
+systemctl daemon-reload
+systemctl enable consul
+systemctl start consul
+systemctl status consul
+
+#ExecStart=/usr/local/bin/consul agent -config-dir=/etc/consul.d/
+#ExecReload=/usr/local/bin/consul reload
