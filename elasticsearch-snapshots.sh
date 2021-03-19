6b42198060b24335cd8309a8900818095964785e
lizarragarigoberto <lizarraga.rigoberto@gmail.com>
2021-03-19 00:25:21 -0600

    Create elasticsearch-snapshots.sh
    elasticsearch migration

---
+++ elasticsearch-snapshots.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+echo "change directory"
+cd /opt/elasticsearch/bin
+pwd
+
+echo "install repository-s3 plugin"
+sudo ./elasticsearch-plugin install repository-s3
+
+echo "configure credentials"
+echo -n 'AKIASR4FY6VNRBLA5W4R' | ./elasticsearch-keystore add --stdin s3.client.default.access_key
+echo -n '2GYHUNy1HA1jmj0hwo0t1SNNA34RZq00XDobqEZz' | ./elasticsearch-keystore add --stdin s3.client.default.secret_key
+
+echo "change permissions"
+sudo chown booxware:booxware /opt/elasticsearch/config/elasticsearch.keystore
+
+
+echo "restart service"
+sudo systemctl restart elasticsearch
+sudo systemctl status elasticsearch
+
+
+echo "create repository"
+curl -XPUT 'http://localhost:9200/_snapshot/s3_repository' -H 'Content-Type: application/json' -d'
+{
+  "type": "s3",
+  "settings": {
+    "bucket": "poc-awsinsagnt",
+    "region": "us-east-1",
+    "client": "default",
+    "endpoint": "s3.us-east-1.amazonaws.com"
+  }
+}'
+
+
+echo "list repositories"
+curl -X GET "http://localhost:9200/_cat/repositories?v"
+
+echo "verify repositories"
+curl -X POST "http://localhost:9200/_snapshot/s3_repository/_verify"
+
+echo "create snapshot"
+ curl -XPUT "http://localhost:9200/_snapshot/s3_repository/gmsgrouptest?wait_for_completion=true" -H 'Content-Type: application/json' -d'
+ {
+    "indices": "gms_group",
+     "ignore_unavailable": true,
+    "include_global_state": false
+ }'
